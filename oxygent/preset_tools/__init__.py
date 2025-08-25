8942d4dfba16acc61cf96921d62d29bc30bac172
Ethon <huangxiaohen2738@gmail.com>
2025-08-25 16:58:03 +0800

    add python and shell tools

--- oxygent/preset_tools/__init__.py
+++ oxygent/preset_tools/__init__.py
@@ -1,5 +1,13 @@
 from .file_tools import file_tools
 from .math_tools import math_tools
+from .python_tools import python_tools
+from .shell_tools import shell_tools
 from .time_tools import time_tools
 
-__all__ = ["math_tools", "file_tools", "time_tools"]
+__all__ = [
+  "math_tools",
+  "file_tools",
+  "python_tools",
+  "shell_tools",
+  "time_tools"
+]
