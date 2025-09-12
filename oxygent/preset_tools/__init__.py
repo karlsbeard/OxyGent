2586f980a9f299ea65ef92c84dd9319e85552f62
zhangjuncai8 <zhangjuncai8@jd.com>
2025-09-12 15:54:45 +0800

    sub 3 tools and testing

--- oxygent/preset_tools/__init__.py
+++ oxygent/preset_tools/__init__.py
@@ -1,5 +1,8 @@
 from .file_tools import file_tools
 from .math_tools import math_tools
 from .time_tools import time_tools
+from .http_tools import http_tools  # 新增
+from .string_tools import string_tools  # 新增
+from .system_tools import system_tools  # 新增
 
-__all__ = ["math_tools", "file_tools", "time_tools"]
+__all__ = ["math_tools", "file_tools", "time_tools", "http_tools", "string_tools", "system_tools"]
